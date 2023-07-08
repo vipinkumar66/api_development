bcc1d716f547d08fe3f09227f4db2ba270ad588b
vipin <vipinkumarlamba55@gmail.com>
2023-07-08 09:31:02 +0530

    Added route for updating the post

--- app/main.py
+++ app/main.py
@@ -1,16 +1,15 @@
 from fastapi import (FastAPI,HTTPException, status,
                      Response, Depends)
-from fastapi.params import Body
-from pydantic import BaseModel
-from typing import Optional
-from random import randrange
+from sqlalchemy.orm import Session
+
 import models, schemas
 from database import engine, get_db
-from sqlalchemy.orm import Session
+
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
 
 @app.get('/sqlalchemy')
 def getall_posts(db:Session = Depends(get_db)):
@@ -21,6 +20,7 @@ def getall_posts(db:Session = Depends(get_db)):
     posts = db.query(models.Post).all()
     return {"data":posts}
 
+
 @app.post('/sqlalchemy/createpost')
 def create_new_post(post:schemas.PostBase, db:Session=Depends(get_db)):
     # new_post = models.Post(title = post.title, content=post.content, published=post.published)
@@ -30,6 +30,7 @@ def create_new_post(post:schemas.PostBase, db:Session=Depends(get_db)):
     db.refresh(new_post)
     return {"post":new_post}
 
+
 @app.get('/sqlalchemy/posts/<int:pk>')
 def get_individual_post(id:int, db:Session=Depends(get_db)):
     post = db.query(models.Post).filter(models.Post.id == id).first()
@@ -37,6 +38,7 @@ def get_individual_post(id:int, db:Session=Depends(get_db)):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
     print(post)
     return {"data":post}
+
 
 @app.get('/sqlalchemy/post/delete/<id:int>')
 def delete_post(id:int, db:Session=Depends(get_db)):
@@ -46,3 +48,14 @@ def delete_post(id:int, db:Session=Depends(get_db)):
     post.delete(synchronize_session=False)
     db.commit()
     return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+
+@app.put('/sqlalchemy/post/update/<id:int>')
+def update_post(id:int, post:schemas.PostBase, db:Session=Depends(get_db)):
+    post_query = db.query(models.Post).filter(models.Post.id == id)
+    first_post = post_query.first()
+    if first_post is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+    post_query.update(post.dict(), synchronize_session=False)
+    db.commit()
+    return {"updated_post":post_query.first()}
